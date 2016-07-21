085079da225b0e9abdaeb4aff3e3d215a41d90ad
victor.govorov <victor.govorov@engine6.com>
2016-07-21 23:25:34 +0500

    added OrSpecification

--- src/Quarks.Specifications/ExpressionExtensions.cs
+++ src/Quarks.Specifications/ExpressionExtensions.cs
@@ -5,7 +5,8 @@ namespace Quarks.Specifications
 {
 	internal static class ExpressionExtensions
 	{
-		public static Expression<Func<T, bool>> AndAlso<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+		public static Expression<Func<T, bool>> AndAlso<T>(
+			this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
 		{
 			ParameterExpression param = expr1.Parameters[0];
 			if (ReferenceEquals(param, expr2.Parameters[0]))
@@ -13,6 +14,17 @@ namespace Quarks.Specifications
 				return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, expr2.Body), param);
 			}
 			return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, Expression.Invoke(expr2, param)), param);
+		}
+
+		public static Expression<Func<T, bool>> OrElse<T>(
+			this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+		{
+			ParameterExpression param = expr1.Parameters[0];
+			if (ReferenceEquals(param, expr2.Parameters[0]))
+			{
+				return Expression.Lambda<Func<T, bool>>(Expression.OrElse(expr1.Body, expr2.Body), param);
+			}
+			return Expression.Lambda<Func<T, bool>>(Expression.OrElse(expr1.Body, Expression.Invoke(expr2, param)), param);
 		}
 	}
 }
---
+++ src/Quarks.Specifications/OrSpecification.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace Quarks.Specifications
+{
+	/// <summary>
+	/// A Logic OR Specification
+	/// </summary>
+	/// <typeparam name="T">Type of entity that check this specification</typeparam>
+	public sealed class OrSpecification<T> : CompositeSpecification<T>
+		where T : class
+	{
+		/// <summary>
+		/// Default constructor for AndSpecification
+		/// </summary>
+		/// <param name="leftSide">Left side specification</param>
+		/// <param name="rightSide">Right side specification</param>
+		public OrSpecification(Specification<T> leftSide, Specification<T> rightSide)
+			: base(leftSide, rightSide)
+		{
+			Expression = leftSide.Expression.OrElse(rightSide.Expression);
+		}
+
+		public static Specification<T> Multiple(params Specification<T>[] specifications)
+		{
+			if (specifications.Length == 0)
+				return new FalseSpecification<T>();
+			if (specifications.Length == 1)
+				return new TrueSpecification<T>();
+
+			Specification<T> result = specifications[0];
+
+			return specifications.Skip(1)
+				.Aggregate(result,
+					(current, spec) => new OrSpecification<T>(current, spec));
+		}
+	}
+}
