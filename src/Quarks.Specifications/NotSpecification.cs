2e3100030e9f077652e4201eba6ebeea90b48296
victor.govorov <victor.govorov@engine6.com>
2016-07-21 23:02:05 +0500

    added NotSpecification

---
+++ src/Quarks.Specifications/NotSpecification.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Quarks.Specifications
+{
+	/// <summary>
+	/// NotSpecification convert a original
+	/// specification with NOT logic operator
+	/// </summary>
+	/// <typeparam name="TEntity">Type of element for this specificaiton</typeparam>
+	public sealed class NotSpecification<TEntity> : Specification<TEntity>
+		where TEntity : class
+	{
+		private Specification<TEntity> _originalSpecification;
+
+		/// <summary>
+		/// Constructor for NotSpecificaiton
+		/// </summary>
+		/// <param name="originalSpecification">Original specification</param>
+		public NotSpecification(Specification<TEntity> originalSpecification)
+		{
+			OriginalSpecification = originalSpecification;
+		}
+
+		/// <summary>
+		/// Constructor for NotSpecification
+		/// </summary>
+		/// <param name="originalSpecification">Original specificaiton</param>
+		public NotSpecification(Expression<Func<TEntity, bool>> originalSpecification)
+		{
+			OriginalSpecification = new DirectSpecification<TEntity>(originalSpecification);
+		}
+
+		public Specification<TEntity> OriginalSpecification
+		{
+			get { return _originalSpecification; }
+			private set
+			{
+				if (value?.Expression == null) throw new ArgumentNullException(nameof(OriginalSpecification));
+
+				_originalSpecification = value;
+
+				Expression<Func<TEntity, bool>> originalCriteria = value.Expression;
+
+				Expression =
+					System.Linq.Expressions.Expression.Lambda<Func<TEntity, bool>>(
+						System.Linq.Expressions.Expression.Not(originalCriteria.Body), originalCriteria.Parameters.Single());
+			}
+		}
+	}
+}
---
+++ tests/Quarks.Specifications.Tests/NotSpecificationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Quarks.Specifications.Tests
+{
+	[TestFixture]
+	public class NotSpecificationTests
+	{
+		[Test]
+		public void Can_Be_Constructed_With_Specification()
+		{
+			var originalSpec = new TrueSpecification<object>();
+
+			var spec = new NotSpecification<object>(originalSpec);
+
+			Assert.That(spec.OriginalSpecification, Is.EqualTo(originalSpec));
+		}
+
+		[TestCase(true, ExpectedResult = false)]
+		public bool IsSatisfied_Reverses_Original_One(bool origResult)
+		{
+			var originalSpec = new DirectSpecification<object>(obj => origResult);
+
+			var spec = new NotSpecification<object>(originalSpec);
+
+			return spec.IsSatisfiedBy(new object());
+		}
+
+		[Test]
+		public void Can_Be_Constructed_With_Expression()
+		{
+			var spec = new NotSpecification<object>(obj => true);
+
+			Assert.That(spec.IsSatisfiedBy(new object()), Is.False);
+		}
+	}
+}
