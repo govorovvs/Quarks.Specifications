aff43a36d9ced94a72db2628e9706fcd1dd3822a
victor.govorov <victor.govorov@engine6.com>
2016-07-21 23:37:55 +0500

    added SpecificationExtensions

---
+++ src/Quarks.Specifications/SpecificationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Quarks.Specifications
+{
+	public static class SpecificationExtensions
+	{
+		public static IEnumerable<T> Where<T>(this IEnumerable<T> collection, Specification<T> spec)
+			where T : class
+		{
+			return collection.Where(spec.Predicate);
+		}
+
+		public static T First<T>(this IEnumerable<T> collection, Specification<T> spec)
+			where T : class
+		{
+			return collection.First(spec.Predicate);
+		}
+
+		public static T FirstOrDefault<T>(this IEnumerable<T> collection, Specification<T> spec)
+			where T : class
+		{
+			return collection.FirstOrDefault(spec.Predicate);
+		}
+	}
+}
---
+++ tests/Quarks.Specifications.Tests/SpecificationExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Quarks.Specifications.Tests
+{
+	[TestFixture]
+	public class SpecificationExtensionsTests
+	{
+		[Test]
+		public void Where()
+		{
+			var spec = new DirectSpecification<string>(str => str.Length >= 2);
+
+			var result = new[] { "aa", "a", "", "aaa" }.Where(spec).ToArray();
+
+			Assert.That(result, Is.EquivalentTo(new[] { "aa", "aaa" }));
+		}
+
+		[Test]
+		public void First()
+		{
+			var spec = new DirectSpecification<string>(str => str.Length > 2);
+
+			var result = new[] { "aa", "a", "aaa", "aa" }.First(spec);
+
+			Assert.That(result, Is.EqualTo("aaa"));
+		}
+
+		[Test]
+		public void FirstOrDefault()
+		{
+			var spec = new DirectSpecification<string>(str => str.Length > 2);
+
+			var result1 = new[] { "aa", "a", "aaa", "aa" }.FirstOrDefault(spec);
+			var result2 = new[] { "aa" }.FirstOrDefault(spec);
+
+			Assert.That(result1, Is.EqualTo("aaa"));
+			Assert.That(result2, Is.Null);
+		}
+	}
+}
