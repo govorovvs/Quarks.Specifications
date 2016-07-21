ee4290f3dac517fd6bf5a5f0461085330e19de61
victor.govorov <victor.govorov@engine6.com>
2016-07-21 23:01:52 +0500

    added DirectSpecification

---
+++ tests/Quarks.Specifications.Tests/DirectSpecificationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Quarks.Specifications.Tests
+{
+	[TestFixture]
+	public class DirectSpecificationTests
+	{
+		[Test]
+		public void Can_Be_Constructed_With_Expression()
+		{
+			var spec = new DirectSpecification<object>(obj => true);
+
+			Assert.That(spec.Predicate.ToString(), Is.EqualTo(new Func<object, bool>(o => true).ToString()));
+		}
+
+		[Test]
+		public void IsSatisfied_Calls_To_Expression()
+		{
+			var spec = new DirectSpecification<object>(obj => true);
+
+			Assert.That(spec.IsSatisfiedBy(new object()), Is.True);
+		}
+	}
+}
