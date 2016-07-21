576afd4b1389a003e0e3c8791df61b7ca7e8b36e
victor.govorov <victor.govorov@engine6.com>
2016-07-21 22:49:51 +0500

    added TrueSpecification

---
+++ src/Quarks.Specifications/TrueSpecification.cs
@@ -0,0 +1,19 @@
+﻿namespace Quarks.Specifications
+{
+	/// <summary>
+	/// True specification
+	/// </summary>
+	/// <typeparam name="TEntity">Type of entity in this specification</typeparam>
+	public sealed class TrueSpecification<TEntity> : Specification<TEntity>
+		where TEntity : class
+	{
+		public TrueSpecification()
+		{
+			//Create "result variable" transform adhoc execution plan in prepared plan
+			//for more info: http://geeks.ms/blogs/unai/2010/07/91/ef-4-0-performance-tips-1.aspx
+			const bool result = true;
+
+			Expression = (t => result);
+		}
+	}
+}
---
+++ tests/Quarks.Specifications.Tests/TrueSpecificationTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Quarks.Specifications.Tests
+{
+	[TestFixture]
+	public class TrueSpecificationTests
+	{
+		[Test]
+		public void ArgumentLess_Constructor()
+		{
+			var spec = new TrueSpecification<object>();
+
+			Assert.That(spec, Is.Not.Null);
+		}
+
+		[Test]
+		public void IsSatisfied_Always_True()
+		{
+			var spec = new TrueSpecification<object>();
+
+			Assert.That(spec.IsSatisfiedBy(new object()), Is.True);
+			Assert.That(spec.IsSatisfiedBy(null), Is.True);
+		}
+	}
+}
