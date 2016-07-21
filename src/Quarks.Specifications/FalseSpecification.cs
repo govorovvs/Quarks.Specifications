ba870ef0d5680dc31a19dbeef727717e695776ea
victor.govorov <victor.govorov@engine6.com>
2016-07-21 22:52:17 +0500

    added FalseSpecification

---
+++ src/Quarks.Specifications/FalseSpecification.cs
@@ -0,0 +1,19 @@
+﻿namespace Quarks.Specifications
+{
+	/// <summary>
+	/// False specification
+	/// </summary>
+	/// <typeparam name="TEntity">Type of entity in this specification</typeparam>
+	public sealed class FalseSpecification<TEntity> : Specification<TEntity>
+		where TEntity : class
+	{
+		public FalseSpecification()
+		{
+			//Create "result variable" transform adhoc execution plan in prepared plan
+			//for more info: http://geeks.ms/blogs/unai/2010/07/91/ef-4-0-performance-tips-1.aspx
+			const bool result = false;
+
+			Expression = t => result;
+		}
+	}
+}
---
+++ tests/Quarks.Specifications.Tests/FalseSpecificationTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Quarks.Specifications.Tests
+{
+	[TestFixture]
+	public class FalseSpecificationTests
+	{
+		[Test]
+		public void ArgumentLess_Constructor()
+		{
+			var spec = new FalseSpecification<object>();
+
+			Assert.That(spec, Is.Not.Null);
+		}
+
+		[Test]
+		public void IsSatisfied_Always_False()
+		{
+			var spec = new FalseSpecification<object>();
+
+			Assert.That(spec.IsSatisfiedBy(new object()), Is.False);
+			Assert.That(spec.IsSatisfiedBy(null), Is.False);
+		}
+	}
+}
